029c32b0669cbe0bf9deba7a9820175069be1706
pepaczz <josef.brechler@gmail.com>
2018-03-25 15:02:36 +0200

    Updated server.R and ui.R as downloaded from https://josefbre.shinyapps.io/Warhammer_probCalculator_v_0_1/, which contains the latest version.

--- server.R
+++ server.R
@@ -1,8 +1,19 @@
+# loadData <- function(){
+#       # require(xlsx)
+#       # require(memisc)  # perhaps not needed?
+#       # setwd("C:/Dropbox/Warhammer")
+#       # result<-read.xlsx("wh_data_v01.xlsx", 1,stringsAsFactors=FALSE)
+#       result<- read.csv2("GitApp/wh_data_v01.csv", stringsAsFactors=FALSE)
+#       return(result)
+# }
+require(ggplot2)
+
 loadData <- function(){
-      require(xlsx)
+      # require(xlsx)
       # require(memisc)  # perhaps not needed?
       # setwd("C:/Dropbox/Warhammer")
-      result<-read.xlsx("wh_data_v01.xlsx", 1,stringsAsFactors=FALSE)
+      # result<-read.xlsx("wh_data_v01.xlsx", 1,stringsAsFactors=FALSE)
+      result<- read.csv2("https://raw.githubusercontent.com/pepaczz/warhammer/master/wh_data_v01.csv", stringsAsFactors=FALSE)
       return(result)
 }
 
@@ -10,8 +21,6 @@ loadData <- function(){
 
 hitMatrix <- function(i,j){
       # to hit matix (attacker in rows)
-      # RETURNS HOW MANY PASSES, NOT HOW MUCH TO BE ROLLED
-      # I.E. 2+ THROW RETURNS 5 
       mat <- matrix(3,10,10)
       for(k in 1:10){
             a<-rep(3,10)
@@ -19,7 +28,7 @@ hitMatrix <- function(i,j){
             a[(1+k*2):(10+k*2)] <- 5
             mat[k,] <- a[1:10]
       }
-      return(7-mat[i,j])
+      return(mat[i,j])
 }  
 
 ################################################################
@@ -37,86 +46,78 @@ woundMatrix <- function(i,j){
             # a[(k+5):(k+10)]
             mat[k,] <- a[3:12]
       }
-      return(7-mat[i,j])
+      return(mat[i,j])
 }
 
 ################################################################
 
-
-getKillProb <- function(a,d,weapMod = 0){
-      ### MODIFIED STRENGTH (WEAPON)
-      str <- a$s + weapMod
-      
+getKillProb <- function(a,d){
       ### TO HIT
-      hit <- hitMatrix(a$ws,d$ws)
-      
-      ### POISONED ATTACKS
-#       pa <- 0
-#       if(a$poison == 1){
-#             hit <- hit - 1
-#             pa <- 1
-#       }
-      
+      hit <- 7 - hitMatrix(a$ws,d$ws)
       ### TO WOUND
-      wound <- woundMatrix(str,d$t)
-      
-      ### KILLING BLOW
-      kb <- 0
-      if(a$killb == 1){
-            wound <- wound - 1
-            kb <- 1
-      }
-      
+      wound <- 7 - woundMatrix(a$s,d$t)
       ### SAVE
       # calculated reciprocaly, i.e. from attacker's point of view
-      saveModif <- max(0, str-3)
+      saveModif <- max(0, a$s-3)
       save <- min(6, d$save - 1 + saveModif)
-      
       ### WARD SAVE / REGENERATION
       # calculated reciprocaly, i.e. from attacker's point of view
       wsave <- min(6, d$wsave - 1)
       
       ### RESULT
-      result <- (((hit-a$poison)*wound)/36+a$poison/6) * (save*wsave/36) + (kb/(6^2))
+      result <- ((hit-a$poison)*(wound-a$killb) * save * wsave) / (6^4) +
+            (a$poison*save*wsave)/(6^3) + ((hit-a$poison)*a$killb*wsave)/(6^3)
+            
       
       return(result)
-      
 }
 
 ################################################################
 
-initializeEmptyDf <- function(){
-      df <-  data.frame(ws=numeric(0),
-                        s=numeric(0),
-                        t=numeric(0),
-                        save=numeric(0),
-                        wsave=numeric(0),
-                        poison=numeric(0),
-                        killb=numeric(0)
-                        )
+evaluateCombatRound <- function(a,d,sampAtt){
+      ### TO HIT
+      toHit <- hitMatrix(a$ws,d$ws)
+      rollh <- sample(6,sampAtt,replace=T)
+      # poisoned attacks
+      rollh_6 <- 0
+      if(a$poison==1){
+            rollh_6 <- sum(rollh==6)
+            rollh <- rollh[! rollh %in% 6]
+      }
+      hits <- sum(rollh >= toHit)
+
+      ### TO WOUND
+      toWound <- woundMatrix(a$s,d$t)
+      rollw <- sample(6,hits,replace=T)
+      # killing blow
+      rollw_6 <- 0
+      if(a$killb == 1){
+            rollw_6 <- sum(rollw==6)
+            rollw <- rollw[! rollw %in% 6]
+      }
+      wounds <- sum(rollw >= toWound) + rollh_6
+      
+      ### SAVE
+      defSave <- d$save + max(0,a$s-4)
+      unsaved <- wounds - sum(sample(6,wounds,replace=T) >= defSave)
+      
+      ### WARD SAVE
+      unwsaved <- unsaved - sum(sample(6,unsaved,replace=T) >= d$wsave) + rollw_6
+       
+      return(unwsaved)
+}
+
+################################################################
+
+simulateCombatRound <- function(a,d,nm,sampAtt=100,nSimul=5000){
+      passed <- replicate(nSimul,evaluateCombatRound(a,d,sampAtt))
+      res <- data.frame(passed = passed, name = nm,stringsAsFactors = F)
+      return(res)
 }
 
 ################################################################
 
 shinyServer(function(input, output) {
-      
-      attacker <- reactive({
-            c(input$a1.ws, input$a1.s, input$a1.t)
-            
-      })
-      
-      attacker <- initializeEmptyDf()
-      defender <- initializeEmptyDf()
-      
-#       a1 <- reactive({
-#             as.numeric(c(input$a1.ws, 
-#                          input$a1.s, 
-#                          input$a1.t,
-#                          input$a1.save,
-#                          input$a1.wsave,
-#                          input$a1.poison,
-#                          input$a1.killb))
-#       }) 
       
       a1 <- reactive({
             data.frame(ws = input$a1.ws, 
@@ -138,9 +139,41 @@ shinyServer(function(input, output) {
                        killb = input$d1.killb)
       }) 
       
-      prob <- reactive({ getKillProb(a1(),d1())})
+      prob_ad <- reactive({ getKillProb(a1(),d1())})
+      prob_da <- reactive({ getKillProb(d1(),a1())})
 
-            
       # Show the values using an HTML table
-      output$values <- renderPrint({ (prob()) })
+      # output$prob_ad <- renderPrint({ paste0("Unit 1 attcks Unit 2: ", prob_ad()) })
+      # output$prob_da <- renderPrint({ paste0("Unit 2 attcks Unit 1: ", prob_da()) })
+      output$prob_ad <- renderPrint({ prob_ad() })
+      output$prob_da <- renderPrint({ prob_da() })
+      
+      output$statA <- renderPrint({ a1() })
+      
+      observeEvent(input$render,
+            output$plot1 <- renderPlot({ 
+                  x1 <- isolate(simulateCombatRound(a1(),d1(),nm="player1Attacks"))
+                  x2 <- isolate(simulateCombatRound(d1(),a1(),nm="player2Attacks"))
+                  x <- rbind(x1,x2)
+                  
+                  bins <- max(x$passed) - min(x$passed)
+                  
+                  pl <-ggplot(data=x,aes(passed,fill = name)) +
+                              geom_density(alpha = 0.5)
+                  print(pl)
+            })
+      )
 })
+
+################################################################
+
+initializeEmptyDf <- function(){
+      df <-  data.frame(ws=numeric(0),
+                        s=numeric(0),
+                        t=numeric(0),
+                        save=numeric(0),
+                        wsave=numeric(0),
+                        poison=numeric(0),
+                        killb=numeric(0)
+      )
+}
--- ui.R
+++ ui.R
@@ -6,32 +6,81 @@ textInput3<-function (inputId, label, value = "",...)
 }
 
 shinyUI(bootstrapPage(
-            hr(),
-            fluidRow(
-                  textInput3(inputId="a1.ws", label="WS", value = 3, class="input-small",min = 1, max = 10),
-                  textInput3(inputId="a1.s", label="S", value = 3, class="input-small",min = 1, max = 10),
-                  textInput3(inputId="a1.t", label="T", value = 3, class="input-small",min = 1, max = 10),
-                  # textInput3(inputId="P4", label="A", value = 3, class="input-small",min = 0, max = 10),
-                  # textInput3(inputId="P5", label="HATRED", value = 3, class="input-small",min = 0, max = 10),
-                  # textInput3(inputId="P5", label="FRENZY", value = 3, class="input-small",min = 0, max = 10),
-                  textInput3(inputId="a1.save", label="SAVE", value = 7, class="input-small",min = 1, max = 7),
-                  textInput3(inputId="a1.wsave", label="WSAVE", value = 7, class="input-small",min = 1, max = 7),
-                  textInput3(inputId="a1.poison", label="POISON", value = 0, class="input-small",min = 0, max = 1),
-                  textInput3(inputId="a1.killb", label="KILB", value = 0, class="input-small",min = 0, max = 1)
+      tabsetPanel(
+            tabPanel("main",
+                  hr(),
+                  column(
+                        width=10,offset=1,
+                        h3("Warhammer combat calculator"),
+                        hr(),
+                        h4("Unit 1 stats:"),
+                        fluidRow(
+                              textInput3(inputId="a1.ws", label="WS", value = 3, class="input-small",min = 1, max = 10),
+                              textInput3(inputId="a1.s", label="S", value = 3, class="input-small",min = 1, max = 10),
+                              textInput3(inputId="a1.t", label="T", value = 3, class="input-small",min = 1, max = 10),
+                              textInput3(inputId="a1.save", label="SAVE", value = 7, class="input-small",min = 1, max = 7),
+                              textInput3(inputId="a1.wsave", label="WSAVE", value = 7, class="input-small",min = 1, max = 7),
+                              textInput3(inputId="a1.poison", label="POISON", value = 0, class="input-small",min = 0, max = 1),
+                              textInput3(inputId="a1.killb", label="KILLB", value = 0, class="input-small",min = 0, max = 1)
+                        ),
+                        h4("Unit 2 stats:"),
+                        fluidRow(
+                              textInput3(inputId="d1.ws", label="WS", value = 3, class="input-small",min = 1, max = 10),
+                              textInput3(inputId="d1.s", label="S", value = 3, class="input-small",min = 1, max = 10),
+                              textInput3(inputId="d1.t", label="T", value = 3, class="input-small",min = 1, max = 10),
+                              textInput3(inputId="d1.save", label="SAVE", value = 7, class="input-small",min = 1, max = 7),
+                              textInput3(inputId="d1.wsave", label="WSAVE", value = 7, class="input-small",min = 1, max = 7),
+                              textInput3(inputId="d1.poison", label="POISON", value = 0, class="input-small",min = 0, max = 1),
+                              textInput3(inputId="d1.killb", label="KILLB", value = 0, class="input-small",min = 0, max = 1)
+                        ),
+                        hr(),
+                        h4("Probability of inflicting a wound: "),
+                        h5("Unit 1 attacks Unit 2: "),
+                        textOutput('prob_ad'),
+                        
+                        h5("Unit 2 attacks Unit 1: "),
+                        textOutput('prob_da'),
+                        hr(),
+                        h4("Attack success histogram"),
+                        h5("Probability density plot for number of successful attacks (horizontal axis, in percentage). 
+                           Mean value of the distibution is the probability above."),
+                        h5("It takes several seconds to render the plot"),
+                        actionButton("render", "render plot"),
+                        plotOutput('plot1')
+                  )
             ),
-            
-            fluidRow(
-                  textInput3(inputId="d1.ws", label="WS", value = 3, class="input-small",min = 1, max = 10),
-                  textInput3(inputId="d1.s", label="S", value = 3, class="input-small",min = 1, max = 10),
-                  textInput3(inputId="d1.t", label="T", value = 3, class="input-small",min = 1, max = 10),
-                  # textInput3(inputId="P4", label="A", value = 3, class="input-small",min = 0, max = 10),
-                  # textInput3(inputId="P5", label="HATRED", value = 3, class="input-small",min = 0, max = 10),
-                  # textInput3(inputId="P5", label="FRENZY", value = 3, class="input-small",min = 0, max = 10),
-                  textInput3(inputId="d1.save", label="SAVE", value = 7, class="input-small",min = 1, max = 7),
-                  textInput3(inputId="d1.wsave", label="WSAVE", value = 7, class="input-small",min = 1, max = 7),
-                  textInput3(inputId="d1.poison", label="POISON", value = 0, class="input-small",min = 0, max = 1),
-                  textInput3(inputId="d1.killb", label="KILB", value = 0, class="input-small",min = 0, max = 1)
-            ),
-            verbatimTextOutput('values')
+            tabPanel("readme",
+                  h3("Warhammer tabletop game combat calculator - readme"),
+                  h4("Probability calculation"),
+                  h5("This utility calculates probability of causing a wound according to Warhammer Fantasy Battles 8th edition rules. 
+                  User have to input stats for the two fighting units. 
+                  Resulting probability should be immediatelly displayed in the middle part of the window.The following abbreviations are used:"), 
+                  h5("WS-weapon skill, S-strength, T-toughness, SAVE-armour save, WSAVE-ward save, POISON-poisonous attacks, KILLB-killing blow."),
+
+#                   tags$ul(
+#                       tags$li(), 
+#                       tags$li("Second list item"), 
+#                       tags$li("Third list item")
+#                   ),
+                  h5("In reality each player rolls consecutively several rolls on six-sided dices and compare the rolls with tabeled needed values. 
+                     Here the probability is calculated using the following formula:"),
+                     
+                     tags$code("result <- ((hit-poison)*(wound-killb) * save * wsave) / (6^4) + "),
+                     br(),
+                     tags$code("(poison*save*wsave)/(6^3) + ((hit-poison)*killb*wsave)/(6^3)"),
+                     
+                     h5("where: "),
+
+                     tags$ol(
+                           tags$li("hit and wound are numbers of dice values connected to a success (e.g. equals to 5 when rolling on 2+) "), 
+                           tags$li("save and wsave are respective values of both saving throws minus 1"), 
+                           tags$li("killb and poison are 0/1 variables")),
+                     
+                     h4("Probability density plot"),
+                     h5("Probability density is displayed for all possible percentage values of successful attacks with nonzero value of density.
+                        Underlying data are not calculated analytically, but they are sampled using a random numbers generator.
+                        There are 5000 samples of 100 attacks used in to calculate the probability.")
+                    
+            )
       )
-)
+))
